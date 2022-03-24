30cf6e91ba824a4e08bd80fde1d6eab288fc4ebf
keonwoo98 <keonwoo98@naver.com>
2022-03-24 16:38:34 +0900

    02. if문

---
+++ 02_if문/Alarm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int h, m;
+
+	cin >> h >> m;
+	if (m >= 45)
+		m -= 45;
+	else
+	{
+		m += 15;
+		if (h == 0)
+			h = 23;
+		else
+			h--;
+	}
+	cout << h << " " << m << endl;
+}
