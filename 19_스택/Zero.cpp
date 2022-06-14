c36f17f0039bc9efb8552f7a2026e18d7919cf9d
keonwoo98 <keonwoo98@naver.com>
2022-06-14 21:11:18 +0900

    19. 스택

--- 19_스택/Zero.cpp
+++ 19_스택/Zero.cpp
@@ -1,23 +1,51 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main(void)
+// {
+// 	int K, num, sum;
+// 	vector<int> v;
+
+// 	cin >> K;
+// 	while (K--)
+// 	{
+// 		cin >> num;
+// 		if (!num)
+// 			v.pop_back();
+// 		else
+// 			v.push_back(num);
+// 	}
+// 	sum = 0;
+// 	for (int i = 0; i < v.size(); i++)
+// 		sum += v[i];
+// 	cout << sum << endl;
+// }
+
+
 #include <iostream>
-#include <vector>
+#include <stack>
 using namespace std;
 
 int main(void)
 {
 	int K, num, sum;
-	vector<int> v;
+	stack<int> s;
 
 	cin >> K;
 	while (K--)
 	{
 		cin >> num;
 		if (!num)
-			v.pop_back();
+			s.pop();
 		else
-			v.push_back(num);
+			s.push(num);
 	}
 	sum = 0;
-	for (int i = 0; i < v.size(); i++)
-		sum += v[i];
+	while (!s.empty())
+	{
+		sum += s.top();
+		s.pop();
+	}
 	cout << sum << endl;
 }
