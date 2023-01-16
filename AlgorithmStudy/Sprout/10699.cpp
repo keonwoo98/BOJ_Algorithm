cb2a1bbafbfc107c86b363be1a97910771c52f61
keonwoo98 <keonwoo98@naver.com>
2023-01-16 23:59:15 +0900

    알고리즘 스터디: 새싹(출력)

---
+++ AlgorithmStudy/Sprout/10699.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+	time_t timer;
+	struct tm* t;
+	timer = time(NULL);
+	t = localtime(&timer);
+
+	int y = t->tm_year + 1900;
+	int m = t->tm_mon + 1;
+	int d = t->tm_mday;
+
+	cout << y;
+	cout << '-';
+	m / 10 == 1 ? cout << m : cout << '0' << m;
+	cout << '-';
+	d / 10 == 1 ? cout << d : cout << '0' << d;
+	cout << endl;
+}
